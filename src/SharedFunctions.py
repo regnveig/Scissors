1b292787383de4ae4c586ba25197d0a074ab0364
Emil Valeev <regnveig@yandex.ru>
2020-12-22 18:48:58 +0700

    -

--- src/SharedFunctions.py
+++ src/SharedFunctions.py
@@ -72,8 +72,8 @@ def Threading(Name, Logger, Threads):
 	StartTime = time.time()
 	
 	# Pooling
-	pool = Pool(Threads)
-	yield pool
+	pool = Pool(11)
+	pool.map(functools.partial(ContigHaplotypeCalling, InputBAM=InputBAM, TempDir=TempDir, Reference=Reference, Logger=Logger, Env=Env), Contigs)
 	pool.close()
 	pool.join()
 	del pool
